8af819c84fe9a268557e1b3440fcfaadc9fbd347
Ubuntu <feather@NeuralFeather.mnvmeltnkwqubavofeectvoi3c.bx.internal.cloudapp.net>
2021-03-19 16:55:50 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ubuntu:18.04
+RUN apt-get update -y
+RUN apt-get install -y python3-pip python3-dev build-essential
+COPY . /app
+WORKDIR /app
+RUN pip3 install --upgrade pip
+RUN pip3 install --upgrade cython 
+RUN pip3 install -r requirements.txt
+RUN pip3 install torch==1.8.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+RUN apt-get install -y p7zip-full && apt-get install -y wget
+RUN wget https://cloud.feather.cloudns.cl/s/B9iYgYAYgan6fWD/download && 7z x download && mv checkpoint-3740000/* output_yt/m/ && rm download
+ENV PYTHONIOENCODING=UTF-8
+ENTRYPOINT ["python3"]
+CMD ["app.py"]
---
+++ app.py
@@ -0,0 +1,45 @@
+#!/usr/local/bin/python3
+# coding: utf8
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+from run_generation2 import generate_text, load_model
+
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+
+# static information as metric
+metrics.info('app_info', 'Application info', version='2.3')
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+model, tokenizer = load_model()
+
+@app.route("/generate", methods=['POST'])
+@cross_origin()
+def get_gen():
+    data = request.get_json()
+
+    if 'text' not in data or len(data['text']) == 0 or 'model' not in data:
+        abort(400)
+    else:
+        text = data['text']
+        #model = data['model']
+
+        result = generate_text(
+            model,
+            tokenizer,
+            model_type='gpt2',
+            length=20,
+            prompt=text,
+            temperature=0.9
+            )
+
+        return jsonify({'result': result})
+
+
+app.run(debug=False,host='0.0.0.0', port = 5001)
